cef769ab47a47d05845e7d35f183b25d4b1aea51
Nick Mai <nick.mai@klook.com>
2019-02-05 15:33:25 +0800

    test ping

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+	"user/conf"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("serser start...")
+	r := mux.NewRouter()
+	r.HandleFunc("/pong", Pong).Methods(http.MethodGet)
+	http.Handle("/", r)
+
+	server := http.Server{
+		Addr:           ":12346",
+		ReadTimeout:    6 * time.Second,
+		WriteTimeout:   6 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		conf.Logger().Fatalf("server: start faile : %v\n", err)
+	}
+}
+
+func Pong(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("receive a request from %v\n", r.RemoteAddr)
+	json.NewEncoder(w).Encode("hello")
+}
